1db228eb85dfde0c82cd7fb7cd1061d468205e64
dhairyajain571 <60997101+dhairyajain571@users.noreply.github.com>
2022-08-31 14:21:09 +0530

    socials_update

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {BsInstagram} from 'react-icons/bs'
 function HeaderSocials() {
   return (
     <div className="header__socials">
-        <a href="#linkednin" target="_blank"><BsLinkedin /></a>
-        <a href="#github" target="_blank"><BsGithub /></a>
-        <a href="#insta" target="_blank"><BsInstagram /></a>
+        <a href="https://www.linkedin.com/in/dhairya-kumar-jain-3bb1b8203/" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/dhairyajain571" target="_blank"><BsGithub /></a>
+        <a href="https://www.instagram.com/_dhairya__j?r=nametag" target="_blank"><BsInstagram /></a>
 
     </div>
   )
