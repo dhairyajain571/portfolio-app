a4bb8e0b5a9d330752b26c8498b8aa9070c3cfe8
dhairyajain571 <60997101+dhairyajain571@users.noreply.github.com>
2022-12-07 09:42:47 +0530

    workshop

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import CV from '../../assets/Workshop.zip'
 
 function CTA() {
   return (
     <div className="cta">
-        <a href={CV} className="btn" download>Download Cv</a>
+        <a href={CV} className="btn" download>Download Files</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
