44712ad62bc20266ab2eb0afa96a53b139c4ff8b
dhairyajain571 <60997101+dhairyajain571@users.noreply.github.com>
2022-12-11 10:50:11 +0530

    Revert "workshop"
    This reverts commit a4bb8e0b5a9d330752b26c8498b8aa9070c3cfe8.

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/Workshop.zip'
+import CV from '../../assets/cv.pdf'
 
 function CTA() {
   return (
     <div className="cta">
-        <a href={CV} className="btn" download>Download Files</a>
+        <a href={CV} className="btn" download>Download Cv</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
