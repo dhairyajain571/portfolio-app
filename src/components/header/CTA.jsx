6addbf2319e078aeb745c3efe63659266e3846da
dhairyajain571 <60997101+dhairyajain571@users.noreply.github.com>
2023-05-03 20:49:30 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,6 +6,7 @@ function CTA() {
     <div className="cta">
         <a href={CV} className="btn" download>Download Cv</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        <iframe src="https://webxr.run/ozkJolmAnApWZ" width="100%" height="100%" frameborder="0" loading="lazy" allow="camera; gyroscope; accelerometer; magnetometer; xr-spatial-tracking;"></iframe>
     </div>
   )
 }
